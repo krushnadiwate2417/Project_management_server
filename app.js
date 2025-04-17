1de2e4c181260f959b9071b52d0ee781809f49a5
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-04-17 18:51:10 +0530

    vercel config

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ const projectRoute = require('./src/routes/projectRoute');
 const taskRoute = require('./src/routes/taskRoute');
 
 const app = express();
+const allowedOrigins = ['http://localhost:5173',];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/api/v1/users',userRoute);
 app.use('/api/v1/project',projectRoute);
 app.use('/api/v1/task',taskRoute);
+
+app.use('/',(req,res,next)=>{
+    res.send({
+        status : "success",
+        error : false
+    })
+    next();
+})
+
 
 
 module.exports = app;
